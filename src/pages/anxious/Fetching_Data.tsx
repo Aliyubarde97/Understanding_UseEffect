38611dde4605b1096f8edea0f7ad8771f636aa6e
unknown <aliyubarde97@gmail.com>
2024-08-11 15:47:31 +0100

    handling error

--- src/pages/anxious/Fetching_Data.tsx
+++ src/pages/anxious/Fetching_Data.tsx
@@ -8,16 +8,26 @@ interface user {
 const Fetching_Data = () => {
 
   const [users, setUsers] = useState<user[]>([])
+  const[error, setError] = useState('');
 
   useEffect(() => {
-    axios.get<user[]>('https://jsonplaceholder.typicode.com/users')
+    axios.get<user[]>('https://jsonplaceholder.typicode.com/xusers')
     .then(res => setUsers(res.data))
+    .catch(err => setError(err.message))
     
   }, []);
 
-  return <ul>
-    {users.map(user => <li key={user.id}>{user.name}</li>)}
-  </ul>
+  return (
+    <>
+    {
+      error &&
+      <p className="text-red-500">{error}</p>
+    }
+    {users.map((user) => (
+      <li  key={user.id}>{user.name}</li>
+    ))}
+    </>
+  )
 }
 
 export default Fetching_Data
