11f8f5faf5a514b4e69b6cc4828fe04804811a25
unknown <aliyubarde97@gmail.com>
2024-08-19 13:44:13 +0100

    loader

--- src/pages/anxious/Fetching_Data.tsx
+++ src/pages/anxious/Fetching_Data.tsx
@@ -2,38 +2,50 @@ import axios, { CanceledError } from "axios"
 import { useEffect, useState } from "react"
 
 interface user {
-  id:number,
-  name:string;
+  id: number,
+  name: string;
 }
 const Fetching_Data = () => {
 
   const [users, setUsers] = useState<user[]>([])
-  const[error, setError] = useState('');
+  const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false)
+
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setLoading(true);
     axios
-    .get<user[]>('https://jsonplaceholder.typicode.com/users', { signal:controller.signal})
-    .then(res => setUsers(res.data))
-    .catch(err => 
-    {
-      if (err instanceof CanceledError) return;
-          setError(err.message)})
-    
+      .get<user[]>('https://jsonplaceholder.typicode.com/users', { signal: controller.signal })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false)
+      })
+
+
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false)
+      });
+     
+
 
     return () => controller.abort();
-    
+
   }, []);
 
   return (
     <>
-    {
-      error &&
-      <p className="text-red-500">{error}</p>
-    }
-    {users.map((user) => (
-      <li  key={user.id}>{user.name}</li>
-    ))}
+      { error &&  <p className="text-red-500">{error}</p>}
+      { isLoading && <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-blue-500">isLoading</div>}
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
   )
 }
