92cdc58e376c6d666155502de7658b4eff8ef5a9
unknown <aliyubarde97@gmail.com>
2024-08-23 16:11:30 +0100

    UserCrude

--- src/pages/anxious/Fetching_Data.tsx
+++ src/pages/anxious/Fetching_Data.tsx
@@ -42,20 +42,52 @@ const Fetching_Data = () => {
         .catch(err => {
           setError(err.message);
           setUsers(originalUsers)
-
-        })
+ 
+        })  
       }
+
+      const addUser = () => {
+        const originalUsers =[...users];
+        const newUser = {id: 0, name:'mosh'};
+        setUsers([newUser, ...users])
+         axios.post('https://jsonplaceholder.typicode.com/users', newUser)
+         .then(({data:savedUser}) => setUsers([savedUser, ...users]))
+         .catch(err => {
+          setError(err.message);
+          setUsers(originalUsers);
+         })
+      };
+       const updateUser = (user: User) => {
+        const originalUsers = [...users];
+            const updatedUser = { ...user, name: user.name + '!'}
+            setUsers(users.map(u => u.id === user.id ? updatedUser: u));
+
+            axios.patch('https://jsonplaceholder.typicode.com/users/' + user.id, updateUser)
+            .catch(err => {
+               setError(err.message)
+               setUsers(originalUsers);  
+            })
+       }
+
   return (
     <>
   
       { error &&  <p className="text-red-500">{error}</p>}
       { isLoading && <div className="animate-spin rounded-full h-32 w-32 border-4 border-blue-500 border-t-transparent border-b-transparent mx-auto"></div>}
-
+      <button className="bg-green-400 rounded-md outline" onClick={() =>
+          addUser()} >Add</button>
       <ul className="border">
         {users.map((user) => (
-          <li key={user.id} className="flex justify-content-between">{user.name} 
+          <li key={user.id} className="flex justify-content-between">{user.name}
+          <div className="flex gap-3 justify-content-center">
           <button className="bg-red-400 rounded-md outline" onClick={() =>
-          deleteUser(user)} >Delete</button></li>
+          updateUser(user)} >update</button> 
+          <button className="bg-red-400 rounded-md outline" onClick={() =>
+          deleteUser(user)} >Delete</button>
+          </div>
+          
+          </li>
+          
         ))}
       </ul>
     </>
